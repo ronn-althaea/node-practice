03cc7da707614838242a0ae65621c84dbc2b456b
ronn-althaea <d.50mm.b@gmail.com>
2020-07-04 21:31:26 +0900

    learn props

--- udemy_react_redux_crud_app/src/App.js
+++ udemy_react_redux_crud_app/src/App.js
@@ -1,32 +1,29 @@
 import React from 'react';
 
-// class App extends Component {
-//     render() {
-//         return (
-//             <React.Fragment>
-//                 <label htmlFor="bar">bar</label>
-//                 <input type="text" onChange={() => {console.log("kiyohi-----")}} />
-//             </React.Fragment>
-//         );
-//     }
-// }
 
 const App = () => {
+    const profiles = [
+        {name: "Taro", age: 10},
+        {name: "Hanako", age: 15},
+        {name: "Noname"}
+    ]
     return (
         <div>
-          <Cat />
-          <Cat />
-          <Cat />
-          <Cat />
-          <Cat />
-          <Cat />
-          <Cat />
+            {
+                profiles.map((profile, index) => {
+                    return <User name={profile.name} age={profile.age} key={index} />
+                })
+            }
         </div>
     );
 }
 
-const Cat = () => {
-    return <div>Wan </div>;
+const User = (props) => {
+    return <div>Hi, I am {props.name}, and {props.age} years old !</div>;
+}
+
+User.defaultProps = {
+    age: 1
 }
 
 export default App;
