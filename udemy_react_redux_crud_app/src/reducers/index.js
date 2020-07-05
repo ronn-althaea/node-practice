4ea96ba235f12184cfbe68b31166d197b22bb507
ronn-althaea <d.50mm.b@gmail.com>
2020-07-05 22:05:23 +0900

    create index page

--- udemy_react_redux_crud_app/src/reducers/index.js
+++ udemy_react_redux_crud_app/src/reducers/index.js
@@ -1,5 +1,5 @@
 import  { combineReducers } from 'redux'
-import count from './count'
+import events from './events'
 
 // export default combineReducers({ foo, bar})
-export default combineReducers({ count })
+export default combineReducers({ events })
